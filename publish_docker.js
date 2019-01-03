acc931a51ede2e34870c98500ca6b8b7ae29397e
gengjiawen <technicalcute@gmail.com>
2019-01-03 20:03:57 +0800

    fix repo name.
    Signed-off-by: gengjiawen <technicalcute@gmail.com>

--- publish_docker.js
+++ publish_docker.js
@@ -2,7 +2,7 @@ const execSync = require("child_process").execSync;
 
 function publishDocker() {
   const now = new Date()
-  const repo = `gengjiawen/ndk`
+  const repo = `gengjiawen/android-ndk`
   const finaltag = `${now.getFullYear()}-${now.getMonth()+1}-${now.getDate()}`
   const cmd =`
 docker login -u ${process.env.DOCKER_USER} -p ${process.env.DOCKER_PASS}
